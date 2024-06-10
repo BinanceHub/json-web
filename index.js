d5a4b509629d7bea75b093ce545916d58182aaad
binancehub <binancehub01@outlook.com>
2024-06-10 21:51:27 +0100

    added things

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.get("/", (req, res) => {
   fs.readFile(__dirname + "/" + "links.json", "utf-8", (err, data) => {
     console.log(data);
